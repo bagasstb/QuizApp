0ac7573db5aa5cb66fda8be3f7e17ae5dc25925d
Satrio Teleng Bagaskoro <telengbagaskorosatrio@gmail.com>
2019-02-28 20:10:07 +0700

    Add progrss bar question

--- QuizApp/Controller/ViewController.swift
+++ QuizApp/Controller/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     let allQuestion = QuestionBank()
     var pickedAnswer: Bool = false
     var questionNumber: Int = 0
-    
+    var score: Int = 0
     
     @IBOutlet weak var progressLabel: UILabel!
     @IBOutlet weak var progressBar: UIView!
@@ -22,9 +22,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let firstQuestion = allQuestion.list[0]
-        questionLabel.text = firstQuestion.questionText
+        startOver()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        updateUI()
     }
     
     @IBAction func truePressed(_ sender: UIButton) {
@@ -36,18 +39,21 @@ class ViewController: UIViewController {
     
     @IBAction func falsePressed(_ sender: UIButton) {
         pickedAnswer = false
-        checkAnswer()
         nextQuestion()
+        checkAnswer()
     }
     
     func updateUI() {
-        
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber + 1) / \(allQuestion.list.count) Question"
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
     }
     
     func nextQuestion() {
-        questionNumber+=1
+        questionNumber = questionNumber + 1
         if questionNumber < 13 {
             questionLabel.text = allQuestion.list[questionNumber].questionText
+            updateUI()
         } else {
             showDialog(title: "Awesome", message: "You've finished all the question, do you want to start over?", positive: "Restart")
         }
@@ -56,15 +62,17 @@ class ViewController: UIViewController {
     func checkAnswer() {
         let correctAnswer = allQuestion.list[questionNumber].answer
         if pickedAnswer == correctAnswer {
-            print("YES!")
+            score = score + 1
+            updateUI()
         } else {
-            print("WRONG!")
+            updateUI()
         }
     }
     
     func startOver() {
         questionNumber = 0
         questionLabel.text = allQuestion.list[0].questionText
+        updateUI()
     }
     
     func showDialog(title: String, message: String, positive: String) {
