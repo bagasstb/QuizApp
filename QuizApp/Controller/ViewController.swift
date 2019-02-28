7cb28bf6caed274140363611db80faf4d52d23b4
Satrio Teleng Bagaskoro <telengbagaskorosatrio@gmail.com>
2019-02-28 19:16:02 +0700

    - Add UI constrain - Implement next and restart quiz

--- QuizApp/Controller/ViewController.swift
+++ QuizApp/Controller/ViewController.swift
@@ -22,15 +22,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let firstQuestion = allQuestion.list[0]
+        questionLabel.text = firstQuestion.questionText
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     @IBAction func truePressed(_ sender: UIButton) {
+        pickedAnswer = true
+        checkAnswer()
+        nextQuestion()
         
     }
     
     @IBAction func falsePressed(_ sender: UIButton) {
-        
+        pickedAnswer = false
+        checkAnswer()
+        nextQuestion()
     }
     
     func updateUI() {
@@ -38,15 +45,35 @@ class ViewController: UIViewController {
     }
     
     func nextQuestion() {
-        
+        questionNumber+=1
+        if questionNumber < 13 {
+            questionLabel.text = allQuestion.list[questionNumber].questionText
+        } else {
+            showDialog(title: "Awesome", message: "You've finished all the question, do you want to start over?", positive: "Restart")
+        }
     }
     
     func checkAnswer() {
-        
+        let correctAnswer = allQuestion.list[questionNumber].answer
+        if pickedAnswer == correctAnswer {
+            print("YES!")
+        } else {
+            print("WRONG!")
+        }
     }
     
     func startOver() {
-        
+        questionNumber = 0
+        questionLabel.text = allQuestion.list[0].questionText
+    }
+    
+    func showDialog(title: String, message: String, positive: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let restartAction = UIAlertAction(title: positive, style: .default) { (UIAlertAction) in
+            self.startOver()
+        }
+        alert.addAction(restartAction)
+        present(alert, animated: true, completion: nil)
     }
 }
 
